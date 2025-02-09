bb36117ff3eb9018fcec9712f80834d3ea0adf25
Arthur Barbosa <arthur.bos@hotmail.coma>
2025-02-09 14:02:01 -0300

    add app colors

---
+++ lib/app/core/shared/consts/app_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color get primary => Color(0xFFD01521);
+  static Color get secondaryButton => Color(0xFFB7000C);
+}
---
+++ lib/app/core/shared/consts/image_consts.dart
@@ -0,0 +1,3 @@
+class ImageConsts {
+  static const String logo = 'assets/images/logo.png';
+}
